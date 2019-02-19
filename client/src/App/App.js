56bba84c33308d9dc83276b77b8eb3ccb21aaff6
madara1793 <akagitagenori@gmail.com>
2019-02-19 20:54:44 +0100

    add login/register logic

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -6,18 +6,44 @@ import Navigation from '../components/Navbar/Navigation';
 import Landing from '../components/layout/Landing/Landing';
 import Login from '../components/Auth/Login/Login';
 import Register from '../components/Auth/Register/Register';
+import PrivateRoute from "../components/PrivateRoute/PrivateRoute";
+import Panel from "../components/Panel/Panel";
+import store from '../store';
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "../actions/authActions";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends Component {
   render() {
     return (
      <div>
-        <Switch>
-            <div>
-            <Navigation/>
             <Route exact path="/" component={ Landing }/>
             <Route exact path="/login" component={Login}/>
             <Route exact path="/register" component={Register}/>
-            </div>
+        <Switch>
+            <PrivateRoute exact path="/panel" component={Panel} />
         </Switch>
      </div>
     );
--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../../actions/authActions";
+import classnames from "classnames";
+
 
 class Register extends Component {
   constructor() {
@@ -12,104 +17,145 @@ class Register extends Component {
       errors: {}
     };
   }
-
-onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-
-onSubmit = e => {
-    e.preventDefault();
-
-const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/panel");
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+  onChange = e => {
+      this.setState({ [e.target.id]: e.target.value });
     };
 
-console.log(newUser);
+  onSubmit = e => {
+      e.preventDefault();
+
+      const newUser = {
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password,
+          password2: this.state.password2
+      };
+      this.props.registerUser(newUser, this.props.history);
   };
 
-render() {
-    const { errors } = this.state;
+  render() {
+      const { errors } = this.state;
 
-return (
-      <div className="container">
-        <div className="row">
-          <div className="col-8 offset-2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col-12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p className="text-dark">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col-12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
-                />
-                <label htmlFor="name">Name</label>
-              </div>
-              <div className="input-field col-12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                />
-                <label htmlFor="email">Email</label>
-              </div>
-              <div className="input-field col-12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <div className="input-field col-12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                />
-                <label htmlFor="password2">Confirm Password</label>
-              </div>
+  return (
+        <div className="container">
+          <div className="row">
+            <div className="col-8 offset-2">
+              <Link to="/" className="btn-flat waves-effect">
+                <i className="material-icons left">keyboard_backspace</i> Back to
+                home
+              </Link>
               <div className="col-12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-primary"
-                >
-                  Sign up
-                </button>
+                <h4>
+                  <b>Register</b> below
+                </h4>
+                <p className="text-dark">
+                  Already have an account? <Link to="/login">Log in</Link>
+                </p>
               </div>
-            </form>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className="input-field col-12">
+                  <input
+                    onChange={this.onChange}
+                    value={this.state.name}
+                    error={errors.name}
+                    id="name"
+                    type="text"
+                    className={classnames("", {
+                      invalid: errors.name
+                    })}
+                  />
+                  <label htmlFor="name">Name</label>
+                  <span className="red-text">{errors.name}</span>
+                </div>
+                <div className="input-field col-12">
+                  <input
+                    onChange={this.onChange}
+                    value={this.state.email}
+                    error={errors.email}
+                    id="email"
+                    type="email"
+                    className={classnames("", {
+                      invalid: errors.email
+                    })}
+                  />
+                  <label htmlFor="email">Email</label>
+                  <span className="red-text">{errors.email}</span>
+                </div>
+                <div className="input-field col-12">
+                  <input
+                    onChange={this.onChange}
+                    value={this.state.password}
+                    error={errors.password}
+                    id="password"
+                    type="password"
+                    className={classnames("", {
+                      invalid: errors.password
+                    })}
+                  />
+                  <label htmlFor="password">Password</label>
+                  <span className="red-text">{errors.password}</span>
+                </div>
+                <div className="input-field col-12">
+                  <input
+                    onChange={this.onChange}
+                    value={this.state.password2}
+                    error={errors.password2}
+                    id="password2"
+                    type="password"
+                    className={classnames("", {
+                      invalid: errors.password2
+                    })}
+                  />
+                  <label htmlFor="password2">Confirm Password</label>
+                  <span className="red-text">{errors.password2}</span>
+                </div>
+                <div className="col-12" style={{ paddingLeft: "11.250px" }}>
+                  <button
+                    style={{
+                      width: "150px",
+                      borderRadius: "3px",
+                      letterSpacing: "1.5px",
+                      marginTop: "1rem"
+                    }}
+                    type="submit"
+                    className="btn btn-primary"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
