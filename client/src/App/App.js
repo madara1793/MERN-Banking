8555dca2189378251ac7e2cec06d2a6063a08fdf
madara1793 <akagitagenori@gmail.com>
2019-02-19 17:41:51 +0100

    add forms

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from "react-router-dom";
 
 import Navigation from '../components/Navbar/Navigation';
 import Landing from '../components/layout/Landing/Landing';
+import Login from '../components/Auth/Login/Login';
+import Register from '../components/Auth/Register/Register';
 
 class App extends Component {
   render() {
     return (
      <div>
         <Navigation/>
-        <Landing/>
+        <Switch>
+          <Route exact path="/" component={ Landing }/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+        </Switch>
      </div>
     );
   }
