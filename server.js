33e643717ed57e085147bcb111c537d169d596f0
madara1793 <akagitagenori@gmail.com>
2019-02-19 15:58:07 +0100

    add form validation

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require("passport");
 
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -23,6 +25,15 @@ mongoose.connect(
 )
 .then(() => console.log("MongoDB succesfully connected"))
 .catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
