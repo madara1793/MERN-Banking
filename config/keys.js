267b6d0f49c5dfdaeb0243716a64e1a88eaec25b
madara1793 <akagitagenori@gmail.com>
2019-02-19 14:17:14 +0100

    initial config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://kamil:polska12@ds157964.mlab.com:57964/users",
+    secretOrKey: "secret"
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+//BodyParser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to DB
+mongoose.connect(
+    db,
+    { useNewUrlParser: true }
+)
+.then(() => console.log("MongoDB succesfully connected"))
+.catch(err => console.log(err))
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
