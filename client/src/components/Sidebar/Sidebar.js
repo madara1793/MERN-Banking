d9497e1a43549d0e69bf5c3964539fca778064ba
madara1793 <akagitagenori@gmail.com>
2019-02-20 13:12:49 +0100

    add sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom';
 export default class Sidebar extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <aside>
+           <ul className="nav navbar-nav d-md-down-none">
+              <li className="nav-item px-3">
+                  <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li className="nav-item px-3">
+                  <Link to="/account">Account</Link>
+              </li>
+              <li className="nav-item px-3">
+                  <Link to="/transactions">Transactions</Link>
+              </li>
+              <li className="nav-item px-3">
+                  <Link to="/converter">Convarter</Link>
+              </li>
+              <li className="nav-item px-3">
+                  <Link to="/settings">Settings</Link>
+              </li>
+              <li className="nav-item px-3">
+                  <Link to="/messages">Messages</Link>
+              </li>
+            </ul>
+      </aside>
     )
   }
 }
