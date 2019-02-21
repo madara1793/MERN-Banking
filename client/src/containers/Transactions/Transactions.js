ac78808783d6c1af1767ca5143ffeaad58b83f4f
madara1793 <akagitagenori@gmail.com>
2019-02-21 12:23:12 +0100

    add transactions list

--- client/src/containers/Transactions/Transactions.js
+++ client/src/containers/Transactions/Transactions.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
-import styled from 'styled-components';
-import {Button} from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
 import Modal from '../../components/Modal/Modal';
+import uuid from 'uuid';
 
 export default class Transactions extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            transactions: [
+                {id: uuid(), accountNumber: 123456, name: "Kamil", address: "bg70", reference: "payment", amount: 5000},
+                {id: uuid(), accountNumber: 654321, name: "Kamil", address: "jbl", reference: "payment", amount: 3000}
+            ]
+        }
+    }
   render() {
     return (
       <div>
@@ -33,6 +44,20 @@ export default class Transactions extends Component {
                 <div className="row">
                     <div className="col-md-12">
                         <h1 className="text-white my-3">History of your transactions</h1>
+                    </div>
+                    <div className="col-md-12">
+                        <div className="card">
+                                <div className="card-body">
+                                <ListGroup>
+                                    {this.state.transactions.map(({id, name}) => (
+                                        <ListGroupItem key={id}>{name}</ListGroupItem>
+                                    ))}
+                                </ListGroup>
+                                </div>
+                                <div className="card-footer">
+
+                                </div>
+                        </div>
                     </div>
                 </div>
            </div>
