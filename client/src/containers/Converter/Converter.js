25f56f746e2b3680233f03399cba081b4a24d307
madara1793 <akagitagenori@gmail.com>
2019-02-27 18:13:51 +0100

    add

--- client/src/containers/Converter/Converter.js
+++ client/src/containers/Converter/Converter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 class Converter extends React.Component {
   constructor(props) {
       super(props);
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    mongoURI: 'mongodb://kamil:polska12@ds157964.mlab.com:57964/users',
+    mongoURI: MONGO_URI,
     secretOrKey: process.env.SECRET_KEY
 };
