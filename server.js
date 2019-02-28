6cc08c19ba7537040a1686c388f00201c2238393
madara1793 <akagitagenori@gmail.com>
2019-02-28 13:48:59 +0100

    add

--- server.js
+++ server.js
@@ -41,12 +41,15 @@ app.use("/api/transactions", transactions);
 app.use("/api/messages", messages);
 
 
-app.use(express.static('client/build'));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
